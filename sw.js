e53ec87484e36aced8576e8ec3f5cf31381bf2e8
Yukanaido <fabricioakiraa@gmail.com>
2020-11-06 14:24:25 -0300

    commit service worker

--- sw.js
+++ sw.js
@@ -1,32 +1,20 @@
-var CACHE_NAME = 'fabricio_cache-v1'
-var urlsToCache = [
-    'img/android-icon-192x192.png',
-    'img/apple-icon-144x144.png',
-    'img/favicon-32x32.png',
-    'js/jquery-3.5.1.min.js',
-    'js/scripts.js'
-]
-
-self.addEventListener('install', function(event){
-    //iniciaria os passos para instalação do nosso service worker
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache){
-            console.log('Cache aberto...')
-            return cache.addAll(urlsToCache)
-        })
-    )
-})
-
-self.addEventListener('fetch', function(event){
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response){
-            if(response){
-                return response
-            }
-            return fetch(event.request)
-        })
-    )
-})
-
+var cacheName = 'fabricio_cache-v1'
+var filesToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
