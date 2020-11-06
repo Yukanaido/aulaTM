0f4792eb45301faa5b955aeaf41a9c8e6a106551
Yukanaido <fabricioakiraa@gmail.com>
2020-11-05 23:17:41 -0300

    Commit inicial do projeto.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.btn-find').click(function(e){
+        e.preventDefault()
+
+        alert('Buscando')
+
+        var cep = $('#CEP').val()
+
+        var find = `https://viacep.com.br/ws/${cep}/json/`
+
+        $.ajax({
+            dataType: 'JSON',
+            type: 'GET',
+            assync: true,
+            url: find,
+            success: function(dados){
+                $('#logradouro').val(dados.logradouro)
+                $('#bairro').val(dados.bairro)
+                $('#localidade').val(dados.localidade)
+                $('#uf').val(dados.uf)
+            }
+        })
+    })
+})
